0eac81589283ea4bd91d7829faee82ead8396928
MangoSwirl <collin.camerono@gmail.com>
2021-11-11 22:30:24 -0800

    Hopefully fixes

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    // Not adding `{ audio: true }` since we only want video now
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+    navigator.mediaDevices.getUserMedia({ video: {facingMode: "environment"} }).then(function (stream) {
         let leftVideo = document.getElementById('left-video');
 
-        leftVideo.srcObject = stream;
-        leftVideo.src = window.URL.createObjectURL(stream);
-        leftVideo.play();
-    });
+        // Older browsers may not have srcObject
+		if ("srcObject" in leftVideo) {
+		    leftVideo.srcObject = stream;
+        } else {
+		    // Avoid using this in new browsers
+		    leftVideo.src = window.URL.createObjectURL(stream);
+    	}
+        leftVideo.onloadedmetadata = function(e) {
+            leftVideo.play();
+        };
+    }).catch(err => alert(err));
 
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+    navigator.mediaDevices.getUserMedia({ video: {facingMode: "environment"} }).then(function (stream) {
         let rightVideo = document.getElementById('right-video');
 
-        rightVideo.srcObject = stream;
-        rightVideo.src = window.URL.createObjectURL(stream);
-        rightVideo.play();
-    });
+        // Older browsers may not have srcObject
+		if ("srcObject" in rightVideo) {
+		    rightVideo.srcObject = stream;
+        } else {
+		    // Avoid using this in new browsers
+		    rightVideo.src = window.URL.createObjectURL(stream);
+    	}
+        rightVideo.onloadedmetadata = function(e) {
+            rightVideo.play();
+        };
+    }).catch(err => alert(err));
+} else {
+    alert('Sorry, your browser does not support getUserMedia API');
 }
